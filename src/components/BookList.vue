6da164587b3d8a46be378e8f56cab170992a9be8
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-10-16 17:22:32 +0300

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,12 +1,12 @@
- <template>
-  
-    <br><hr>
-    <book-form @addBook='appendBook'></book-form>
-  </div>
+<template>
   <div>
     <h1>{{title}}</h1>
+   
+  <div>
+    <h1>{{title}}</h1>
+    <input type="text" v-model="searchInput" placeholder="Search Books">
     <ul class="exists">
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
     <h2>Filtered Books by Ownership</h2>
@@ -34,7 +34,7 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-
+        
         {
           title: "Self-Reliance",
           author: "Ralph Waldo Emerson",
@@ -55,18 +55,25 @@ export default {
         }
       ],
       filters: ["bought", "borrowed"],
-      holding: "bought"
+      holding: "bought",
+      searchInput: ""
     };
   },
   components: {
     BookItem,
-   
+    
     BookForm
   },
  
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
@@ -83,10 +90,13 @@ export default {
 </script>
 
 <style>
+
 h1,
 h2 {
   font-weight: normal;
 }
+ul {
+
 ul {
   list-style-type: none;
   padding: 0;
