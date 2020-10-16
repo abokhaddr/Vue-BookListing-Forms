529818178033ed06b022b2ea76db2ec39ff6805d
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-10-16 16:34:27 +0300

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,7 +1,8 @@
-<template>
+  <template>
   <div>
     <h1>{{title}}</h1>
     <ul>
+       
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
     <br><hr>
@@ -10,16 +11,18 @@
 </template>
 
 <script>
+
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
-
 export default {
-  name: "BookList",
+   name: "BookList",
   data() {
     return {
+      
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
+
         { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
         { title: "American Gods", author: "Neil Gaiman" },
         { title: "Amusing Ourselves to Death", author: "Neil Postman" }
@@ -33,12 +36,15 @@ export default {
   methods: {
     appendBook(bookTitle, bookAuthor) {
       this.books.push({ title: bookTitle, author: bookAuthor });
+    
+  } 
     }
   }
 };
 </script>
 
 <style>
+      
 h1,
 h2 {
   font-weight: normal;
@@ -47,7 +53,6 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: block;
   margin: 0 10px;
